45d3bb67ccf3ba073ecd375848d4ec0fe85e90d8
ervnmrcdo <ervinmercado31@gmail.com>
2025-08-29 11:45:16 +0800

    Added colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,76 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1>Group (3) Project Blog</h1>
+      <div className={styles.header}>
+        <h1 className={styles.big_title}>Faculty Records Project Blog</h1>
 
-      <div className={styles.members_clients_container}>
-        <h3>Client</h3>
-        <div className={styles.client}>
-          <p>Ms. Juayong</p>
-        </div>
-        <h3>Members</h3>
-        <div className={styles.members}>
-          <p>Cantero</p>
-          <p>Bugaoan</p>
-          <p>Mercado</p>
+        <div className={styles.members_clients_container}>
+          <div className={styles.people_wrapper}>
+            <h4>Client</h4>
+            <div className={styles.people}>
+              <p>Prof. Juayong</p>
+            </div>
+          </div>
+          <div className={styles.people_wrapper}>
+            <h4>Members</h4>
+            <div className={styles.people}>
+              <p>Bugaoan</p>
+              <p>Cantero</p>
+              <p>Mercado</p>
+            </div>
+          </div>
         </div>
       </div>
+      <hr />
+      <div className={styles.content_container}>
+        <h3>Project Definition</h3>
+        <p>
+          A centralized database for faculty and student records that would
+          scurely store valuable information, aid in tedious document
+          management, and benefit both the administrators, faculty, and
+          studentsa alike
+        </p>
+      </div>
+      <div className={styles.content_container}>
+        <h3>Rationale</h3>
+        <p>
+          The department currently manually stores these records with a mix of
+          Microsoft Word, Google Forms, and individual spreadsheets. This makes
+          for a process that is slow, error-prone, and rigorous. Faculty members
+          would also have to manually input their works, which are often
+          scattered over different websites using different formats such as APA,
+          MLA, and the like
+        </p>
+      </div>
+      <div className={styles.content_container}>
+        <h3>Roadmap</h3>
+        <p></p>
+      </div>
+      <div className={styles.content_container}>
+        <h3>Project Features</h3>
+        <ul>
+          <li>Store valuable information for faculty and students,</li>
+          <li>Secure valuable information for faculty and students,</li>
+          <li>Showcase awards and publications of faculty and students ,</li>
+          <li>
+            Automatically determine the eligibility of faculty and students for
+            various awards,
+          </li>
 
-      <div className={styles.project_plan_file}>
+          <li>
+            Automatically alert the administrator for any eligible awardees
+            within the department,
+          </li>
+          <li>
+            Fill up the corresponding documents for application of eligible
+            awards,
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.content_container}>
         <h3>Project plan</h3>
+        <p>To be added...</p>
       </div>
     </>
   );
